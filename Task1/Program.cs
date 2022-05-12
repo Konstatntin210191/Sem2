b5c255157376c505119413e197a2ea0bd96fcc00
Konstantin <bonne.journee@mail.ru>
2022-05-12 20:26:32 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random().Next(10,100);
+Console.WriteLine($"Сгенерированное число: {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+Console.WriteLine($"Первое число: {firstDigit}, второе число: {secondDigit}");
+
+int max = firstDigit > secondDigit ? firstDigit : secondDigit;
+
+Console.WriteLine($"Наибольшая цифра из чисел: {max}");
